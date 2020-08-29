79653fbd1da3fac336097a881c7ebc75767fc663
iamkaleb <kalebdmoran@gmail.com>
2020-08-29 17:50:08 -0500

    going to try db stuff again

--- src/components/Map.js
+++ src/components/Map.js
@@ -13,44 +13,7 @@ const Map = props => {
     const [shipwrecks, setShipwrecks] = useState([])
 
     useEffect(() => {
-        const results = []
-
-        ApiManager.getList("categorymembers", "max", "Category:Lists_of_shipwrecks_by_location")
-            .then(response => {
-                // console.log("response 1", response)
-                response.query.categorymembers.forEach(category => {
-                    ApiManager.getGenerator("links", "info", category.pageid, "max")
-                        .then(response => {
-                            // console.log("response 2", response)
-                            if (response.query) {
-                                Object.entries(response.query.pages).forEach(entry => {
-                                    // console.log('entry', entry)
-                                    if (entry[1].length) {
-                                        ApiManager.getArticleByProp("coordinates", entry[0])
-                                        .then(response => {
-                                            // console.log("response 3", response)
-                                            Object.entries(response.query.pages).forEach(entry => {
-                                                // console.log("entry2", entry)
-                                                if (entry[1].coordinates) {
-                                                    // console.log("got the coords")
-                                                    results.push({
-                                                        "pageid": entry[0],
-                                                        "coordinates": {
-                                                            "lat": entry[1].coordinates[0].lat,
-                                                            "lon": entry[1].coordinates[0].lon
-                                                        }
-                                                    })
-                                                    // console.log("results prog", results)
-                                                }
-                                            })
-                                            setShipwrecks(results)
-                                        })
-                                    }
-                                })
-                            }
-                        })
-                })
-            })
+        setShipwrecks(getCoordinates())
     }, [])
 
     const toggleModal = () => {
@@ -75,15 +38,15 @@ const Map = props => {
         {showModal
             ? createPortal(<Tooltip />, document.getElementById('modal'))
             : null}
-        {shipwrecks.map(mappedShipwreck =>
+        {/* {shipwrecks.map(mappedShipwreck =>
             <Icon
-            // onClick={toggleModal}
-            key={mappedShipwreck.pageId}
+            onClick={toggleModal}
+            key={mappedShipwreck.pageid}
             lat={mappedShipwreck.coordinates.lat}
             lng={mappedShipwreck.coordinates.lon}
-            $hover={true}
+            // $hover={true}
             />
-        )}
+        )} */}
         </GoogleMapReact>
       </div>
     )
